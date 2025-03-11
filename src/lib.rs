e3498c6d95c119a456fe7e93e04a5820234bb22a
Timm Kühnel <t_kueh06@uni-muenster.de>
2025-03-11 17:51:22 +0100

    Initial Commit & First Working Version of Calculator Implementation
    Inspired by: https://medium.com/@danmugh/rust-for-beginners-dive-into-coding-with-these-5-projects-to-boost-your-skills-7307e7923d74

---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+use std::{f64::INFINITY};
+
+use num_traits::{self, Pow};
+
+pub fn factorial (number:u64) -> u64 {
+    if number < 2 {
+        1
+    } else {
+        number * factorial(number - 1)
+    }
+}
+
+pub struct Calculator {
+    param_a : f64,
+    param_b : f64,
+}
+impl Calculator {
+    // constructor
+    /**
+     * Overdid it here; NumCast appears incompatible with Option type
+     */
+    // pub fn new<T : num_traits::NumCast, S : num_traits::NumCast>(param_a:T, param_b:Option<S>) -> Self {
+    //     Self {
+    //         param_a : num_traits::cast(param_a).unwrap(),
+    //         param_b : match param_b {
+    //             Some(value) => num_traits::cast(value).unwrap(),
+    //             None => INFINITY,
+    //         }
+    //     }
+    // }
+    pub fn new(param_a:f64, param_b:Option<f64>) -> Self {
+        Self {
+            param_a : num_traits::cast(param_a).unwrap(),
+            param_b : match param_b {
+                Some(value) => num_traits::cast(value).unwrap(),
+                None => INFINITY,
+            }
+        }
+    }
+
+    pub fn add(&self) -> f64 {
+        assert_ne!(self.param_b, INFINITY, "no second parameter given, or passed as INFINITY");
+        self.param_a + self.param_b
+    }
+
+    pub fn subtract(&self) -> f64 {
+        assert_ne!(self.param_b, INFINITY, "no second parameter given, or passed as INFINITY");
+        self.param_a - self.param_b
+    }
+
+    pub fn multiply(&self) -> f64 {
+        assert_ne!(self.param_b, INFINITY, "no second parameter given, or passed as INFINITY");
+        self.param_a * self.param_b
+    }
+
+    pub fn divide(&self) -> f64 {
+        assert_ne!(self.param_b, INFINITY, "no second parameter given, or passed as INFINITY");
+        assert_ne!(self.param_b, 0.0, "Division by Zero is illegal!");
+        self.param_a / self.param_b
+    }
+
+    pub fn modulo(&self) -> Result<f64,&str> {
+        assert_ne!(self.param_b, INFINITY, "no second parameter given, or passed as INFINITY");
+        if self.param_b == 0.0 {
+            return Err("X mod 0 is undefined")
+        }
+        Ok(self.param_a % self.param_b)
+    }
+
+    pub fn facto(&self) -> f64 {
+        let param_a_as_int : u64 = self.param_a as u64;
+        assert_eq!(self.param_a, param_a_as_int as f64, "factorial is only implemented for positive int types (no decimals)");
+        let res = factorial(param_a_as_int);
+        res as f64
+    }
+
+    pub fn square(&self) -> f64 {
+        self.param_a.pow(2)
+    }
+
+    pub fn pow(&self) -> f64 {
+        assert_ne!(self.param_b, INFINITY, "no second parameter given, or passed as INFINITY");
+        self.param_a.pow(self.param_b)
+    }
+
+    pub fn sqrt(&self) -> f64 {
+        f64::sqrt(self.param_a)
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,159 @@
+// src/main.rs
+
+/**
+ * Calculator inspired by https://medium.com/@danmugh/rust-for-beginners-dive-into-coding-with-these-5-projects-to-boost-your-skills-7307e7923d74
+ * Extended by some options, structure, overly ambitious (and currently inactive) templating.
+ */
+
+use std::{io,process};
+
+use practice_calculator::Calculator;
+
+fn main() {
+    println!("Simple Calculator");
+    println!("1. Addition:\t\tadd");
+    println!("2. Subtraction:\t\tsub");
+    println!("3. Multiplication\tmult");
+    println!("4. Division:\t\tdiv");
+    println!("5. Modulo:\t\tmod");
+    println!("6. Factorial:\t\tfac");
+    println!("7. Square:\t\tsq");
+    println!("8. Exponentiation:\texp");
+    println!("9. Square Root:\t\tsqrt");
+    println!("0. Exit:\t\tquit\n");
+    loop {
+        println!("Option: ");
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Failed to read line");
+        let choice : &str = choice.trim();
+        if choice.is_empty() {continue;}
+
+        match choice {
+            "add"   => {
+                println!("Addition:\tX + Y");
+            }
+            "sub"   => {
+                println!("Subtraction:\tX - Y");
+            }
+            "mult"  => {
+                println!("Multiplication:\tX * Y");
+            }
+            "div"   => {
+                println!("Division:\tX / Y");
+            }
+            "mod"   => {
+                println!("Modulo:\tX mod Y");
+            }
+            "exp"   => {
+                println!("Exponentiation:\tX ^ Y");
+            }
+            "fac"   => {
+                println!("Factorial:\tX!");
+            }
+            "sq"    => {
+                println!("Square:\tX ^ 2");
+            }
+            "sqrt"  => {
+                println!("Square Root:\tsqrt(X)");
+            }
+            "quit" => {
+                println!("Exiting. Goodbye!");
+                process::exit(0);
+            }
+            _ => {
+                println!("Somehow, an invalid option made it through:{}\nPlease try again or contact the author.\n", choice);
+                continue;
+            }
+        };
+
+        let calc : Calculator =  match choice {
+            "add" | "sub" | "mult" | "div" | "mod" | "exp" => {
+                println!("Enter the first numeric parameter of the equation (X):");
+                let mut num1 = String::new();
+                io::stdin().read_line(&mut num1).expect("Failed to read line");
+                let num1:f64 = match num1.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("Invalid input. Please enter a valid number.");
+                        continue;
+                    }
+                };
+                println!("Enter the second numeric parameter of the equation (Y):");
+                let mut num2 = String::new();
+                io::stdin().read_line(&mut num2).expect("Failed to read line");
+                let num2:f64 = match num2.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("Invalid input. Please enter a valid number.");
+                        continue;
+                    }
+                };
+                Calculator::new(num1, Some(num2))
+            }
+            "fac" | "sq" | "sqrt" => {
+                println!("Enter the numeric parameter of the equation (X):");
+                let mut num = String::new();
+                io::stdin().read_line(&mut num).expect("Failed to read line");
+                let num:f64 = match num.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("Invalid input. Please enter a valid number.");
+                        continue;
+                    }
+                };
+                Calculator::new(num, None)
+            }
+            _ => {
+                println!("Invalid input. Please enter a valid option.");
+                continue;
+            }
+        };
+
+        match choice {
+            "add"   => {
+                let result = calc.add();
+                println!("Result: {}", result);
+            }
+            "sub"   => {
+                let result = calc.subtract();
+                println!("Result: {}", result);
+            }
+            "mult"  => {
+                let result = calc.multiply();
+                println!("Result: {}", result);
+            }
+            "div"   => {
+                let result = calc.divide();
+                println!("Result: {}", result);
+            }
+            "mod"   => {
+                let result = calc.modulo();
+                match result {
+                    Ok(res) => println!("Result: {}", res),
+                    Err(err) => println!("Result: {}", err),
+                };
+            }
+            "exp"   => {
+                let result = calc.pow();
+                println!("Result: {}", result);
+            }
+            "fac"   => {
+                let result = calc.facto();
+                println!("Result: {}", result);
+            }
+            "sq"    => {
+                let result = calc.square();
+                println!("Result: {}", result);
+            }
+            "sqrt"  => {
+                let result = calc.sqrt();
+                println!("Result: {}", result);
+            }
+            _ => {
+                println!("Somehow, an invalid option made it through: {}\nPlease try again or contact the author.", choice);
+            }
+        };
+
+        println!("\nWould you like to perform another calculation?");
+    }
+}
